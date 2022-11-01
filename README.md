1f229ff45f06aebc3c5dc8066555a676586c83a3
Alun Hewinson <alunhewinson@gmail.com>
2022-11-01 16:57:52 +0000

    update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code
+
+See the [Advent of Code](https://adventofcode.com/) site
--- src/main/scala/aoc2021/day01.scala
+++ src/main/scala/aoc2021/day01.scala
@@ -3,6 +3,6 @@ package aoc2021
 object day01 extends App {
   // https://adventofcode.com/2021/day/1
 
-  
+
 
 }
